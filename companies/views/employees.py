61ead52273c4d88286af15386d1d89de01dedb63
itallominatti <itallominatti@gmail.com>
2024-04-24 14:16:53 -0300

    [ feat ] main -> Criando arquivo views base do app companies

---
+++ companies/views/employees.py
@@ -0,0 +1,6 @@
+from companies.views.base import Base
+from companies.utils.permissions import IsOwner
+
+from rest_framework.views import Response, status
+from rest_framework.exceptions import APIException
+
