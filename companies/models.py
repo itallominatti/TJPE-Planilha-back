63663fd63bad80b58cab37d419c515ac1b0467a4
itallominatti <itallominatti@gmail.com>
2024-04-24 12:58:25 -0300

    [ feat ] main -> Criando Model TaskStatus e Task

--- companies/models.py
+++ companies/models.py
@@ -13,4 +13,24 @@ class Employee(models.Model):
 
     def __str__(self):
         return self.name
-# Create your models here.
+
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=155)
+    codename = models.CharField(max_length=155)
+
+    class Meta:
+        db_table = 'companies_task_status'
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.TextField()
+    description = models.TextField(null=True)
+    due_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Tjpe, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
